822059042e654ad18693535730d5f683d0eeebfc
Brett Morris <brettmorris21@gmail.com>
2016-03-30 23:43:09 -0700

    Updaing example file

--- exp.py
+++ exp.py
@@ -1,8 +1,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from shampoo.reconstruction import Hologram
-import matplotlib.pyplot as plt
+from shampoo import Hologram
+
 hologram_path = 'data/Hologram.tif'
 propagation_distance = 0.03685  # m
 
@@ -10,6 +10,7 @@ h = Hologram.from_tif(hologram_path)
 waveform = h.reconstruct(propagation_distance,
                          plot_aberration_correction=False)
 
+import matplotlib.pyplot as plt
 fig, ax = waveform.plot(all=True)
 fig.suptitle("d = {0}".format(propagation_distance))
 plt.show()
