ffa58289c65ae6430770da0b839f5950cc0ef906
Brett Morris <brettmorris21@gmail.com>
2016-04-01 10:46:06 -0700

    Adopting astropy package tempalte

--- shampoo/__init__.py
+++ shampoo/__init__.py
@@ -1,2 +1,12 @@
+#-------------------------------------------------------------
+# astropy package template commands
+from ._astropy_init import *
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+        from example_mod import *
+#-------------------------------------------------------------
+
 from .reconstruction import *
 from .store import *
+
