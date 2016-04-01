b321ab0d9cdec95f1ea5218b3436b62918ae375d
Brett Morris <brettmorris21@gmail.com>
2016-04-01 12:24:08 -0700

    Mocking hdf5, pyfftw

--- docs/conf.py
+++ docs/conf.py
@@ -154,3 +154,16 @@ if eval(setup_cfg.get('edit_on_github')):
 
     edit_on_github_source_root = ""
     edit_on_github_doc_root = "docs"
+
+## -- Mocking --------
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pyfftw', 'h5py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
