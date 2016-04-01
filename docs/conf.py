1ca3fb89f8d72a907d1bfd6fb9688286ab55cd02
Brett Morris <brettmorris21@gmail.com>
2016-04-01 13:20:55 -0700

    rtd fixes

--- docs/conf.py
+++ docs/conf.py
@@ -165,5 +165,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pyfftw', 'h5py']
+MOCK_MODULES = ['pyfftw', 'h5py', 'pyfftw.interfaces.scipy_fftpack']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
