10e2dfd105fcdd288645421fcd7c8ac7d75d7254
Brett Morris <brettmorris21@gmail.com>
2016-04-01 14:03:17 -0700

    trying to dodge recursion in rtd

--- docs/conf.py
+++ docs/conf.py
@@ -162,12 +162,5 @@ try:
 except ImportError:
     from unittest.mock import MagicMock
 
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
-
-MOCK_MODULES = ['pyfftw', 'h5py', 'pyfftw.interfaces.scipy_fftpack',
-                'fftw', 'hdf5']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+MOCK_MODULES = ['pyfftw', 'h5py', 'fftw', 'hdf5']
+sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
