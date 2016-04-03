fc37112360b002a7e7416b97e55881652042dca0
Brett Morris <brettmorris21@gmail.com>
2016-04-03 12:06:26 -0700

    trying h5py in conda dependencies

--- shampoo/__init__.py
+++ shampoo/__init__.py
@@ -4,4 +4,4 @@ from ._astropy_init import *
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
         from .reconstruction import *
-
+        from .store import *
