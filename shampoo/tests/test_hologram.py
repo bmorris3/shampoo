14613fb1d8a5708801531cd0aec99712186f5737
Brett Morris <brettmorris21@gmail.com>
2016-04-04 14:30:54 -0700

    Crop out just centers of holograms for reconstruction

--- shampoo/tests/test_hologram.py
+++ shampoo/tests/test_hologram.py
@@ -2,9 +2,10 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from ..reconstruction import (Hologram, rebin_image, _find_peak_centroid,
-                              RANDOM_SEED)
+                              RANDOM_SEED, _crop_image, CropEfficiencyWarning)
 
 import numpy as np
+import pytest
 np.random.seed(RANDOM_SEED)
 
 
@@ -22,6 +23,7 @@ def _example_hologram(dim=2048):
 
 def test_load_hologram():
     holo = Hologram(_example_hologram())
+    assert holo is not None
 
 
 def test_rebin_image():
@@ -38,6 +40,23 @@ def _gaussian2d(amplitude, width, centroid, dim):
 
 
 def test_centroid():
-    centroid = (65, 35)
-    test_image = _gaussian2d(amplitude=10, width=5, centroid=centroid, dim=100)
+    centroid = (265, 435)
+    test_image = _gaussian2d(amplitude=10, width=5, centroid=centroid, dim=1024)
     assert np.all(_find_peak_centroid(image=test_image) == centroid)
+    assert np.all(test_image[centroid] == np.max(test_image))
+
+
+def test_crop_image():
+    # Even number rows/cols
+    image1 = np.arange(100).reshape((10, 10))
+    new_shape1 = (image1.shape[0]//2, image1.shape[1]//2)
+    with pytest.warns(CropEfficiencyWarning):
+        cropped_image1 = _crop_image(image1, 0.5)
+    assert new_shape1 == cropped_image1.shape
+
+    # Odd number rows/cols
+    image2 = np.arange(121).reshape((11, 11))
+    new_shape2 = (image2.shape[0]//2 + 1, image2.shape[1]//2 + 1)
+    with pytest.warns(CropEfficiencyWarning):
+        cropped_image2 = _crop_image(image2, 0.5)
+    assert new_shape2 == cropped_image2.shape
