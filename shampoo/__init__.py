33d0310671a9d1942b1a84e881922de6d578e09d
Brett Morris <brettmorris21@gmail.com>
2016-04-08 14:59:36 -0700

    Adding vis, focus to imports

--- shampoo/__init__.py
+++ shampoo/__init__.py
@@ -5,5 +5,7 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-        from .reconstruction import *
-        from .store import *
+    from .reconstruction import *
+    from .store import *
+    from .focus import *
+    from .vis import *
